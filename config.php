29469f7f137d5ed514b6d375cec85b1ff5faec6d
Ikhsan129 <109275382+Ikhsan129@users.noreply.github.com>
2022-07-14 16:18:00 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php 
+class Database 
+{ 
+ 
+  var $host = "localhost"; 
+  var $username = "root"; 
+  var $password = ""; 
+  var $database = "nilaiMahasiswa"; 
+  var $koneksi = ""; 
+ 
+  function __construct() 
+  { 
+    $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->database); 
+    if (mysqli_connect_errno()) { 
+      echo "Koneksi database gagal : " . mysqli_connect_error(); 
+    } 
+  } 
+ 
+  function tampil_data() 
+  { 
+    $data = mysqli_query($this->koneksi, "select * from mhs"); 
+    while ($row = mysqli_fetch_assoc($data)) { 
+    $hasil[] = $row; 
+  } 
+return $hasil; 
+}
