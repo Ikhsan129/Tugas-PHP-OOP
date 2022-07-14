002186e36ca8db17f24ab0589651fddc68af04ca
Ikhsan129 <109275382+Ikhsan129@users.noreply.github.com>
2022-07-14 17:01:12 +0700

    Update prose_mhs.php

--- prose_mhs.php
+++ prose_mhs.php
@@ -4,11 +4,21 @@ $db = new Database();
  
 $action = $_GET['action']; 
 if ($action == "add") { 
+ $nim = $_POST['NIM']; 
+ $nama = $_POST['namaMhs']; 
+ $angkatan = $_POST['angkatan']; 
+ $kode_prodi = $_POST['kode_prodi']; 
+  
+ $db->tambah_data($nim, $nama, $angkatan, $kode_prodi); 
+ header('location:tampil_data.php');
+ elseif ($action == "update") { 
+  $id = $_POST['id']; 
   $nim = $_POST['NIM']; 
   $nama = $_POST['namaMhs']; 
   $angkatan = $_POST['angkatan']; 
   $kode_prodi = $_POST['kode_prodi']; 
-  
-  $db->tambah_data($nim, $nama, $angkatan, $kode_prodi); 
+ 
+  $db->update_data($id, $nim, $nama, $angkatan, $kode_prodi); 
   header('location:tampil_data.php'); 
+ }
 }
