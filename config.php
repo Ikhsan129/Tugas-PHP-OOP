07fe96221b5d63928d9ad13481c56e911667e17e
Ikhsan129 <109275382+Ikhsan129@users.noreply.github.com>
2022-07-14 17:06:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,33 +4,6 @@ class Database
  
   var $host = "localhost"; 
   var $username = "root"; 
-  var $password = ""; 
-  var $database = "nilaiMahasiswa"; 
-  var $koneksi = ""; 
- 
-  function __construct() 
-  { 
-    $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->database); 
-    if (mysqli_connect_errno()) { 
-      echo "Koneksi database gagal : " . mysqli_connect_error(); 
-    } 
-  } 
- 
-  function tampil_data() 
-  { 
-    $data = mysqli_query($this->koneksi, "select * from mhs"); 
-    while ($row = mysqli_fetch_assoc($data)) { 
-    $hasil[] = $row; 
-  } 
-return $hasil; 
-}
-
-<?php 
-class Database 
-{ 
- 
-  var $host = "localhost"; 
-  var $username = "root"; 
   var $password = "S3t14w4n"; 
   var $database = "nilaiMahasiswa"; 
   var $koneksi = ""; 
@@ -66,4 +39,9 @@ function get_by_id($id)
 function update_data($id, $nim, $nama, $angkatan, $kode_prodi) 
 { 
  $query = mysqli_query($this->koneksi, "update mhs set NIM='$nim', namaMhs='$nama', angkatan='$angkatan', kode_prodi='$kode_prodi' where id='$id'"); 
+} 
+
+function delete_data($id) 
+{ 
+ $query = mysqli_query($this->koneksi, "delete from mhs where id='$id'"); 
 } 
